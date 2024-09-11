a31c3c6b58169b738aa0f33fb389921cbbdd229f
xchewmer6666 <xchewmer6666@gmail.com>
2024-09-11 15:50:41 +0530

    init1.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,12 +10,29 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+function TopNav() {
+  return (
+    <div className="flex justify-between p-4 text-xl font-semibold">
+      <div>Gallery</div>
+
+      <div>Sign In</div>
+    </div>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${GeistSans.variable} `}>
+      <body className={`font-sans ${inter.className} flex flex-col gap-4`}>
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import Image from "next/image";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://utfs.io/f/cda6bea3-299f-47e8-b9f4-a3bbb5246e43-1x989p.jpeg",
+  "https://utfs.io/f/77943ef1-104c-4635-9b81-69bd6195a376-1x989s.JPG",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
+          (image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} alt={`Image ${image.id}`} />
+            </div>
+          ),
+        )}
+      </div>
     </main>
   );
 }
